0bb74476576bbf2a48435d31fc789cf55293a803
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2024-05-09 21:24:20 +0100

    Just some simple changes

---
+++ GenerateResponse.js
@@ -0,0 +1,33 @@
+
+
+const OpenAI = require('openai');
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // Make sure to set your API key in the environment variables
+});
+
+
+
+async function sendPromptToOpenAI(text) {
+  try {
+
+    const prompt  = `evaluate the profitability and scalability of this business idea ` + text; 
+    console.log('this is the prompt', prompt); 
+    const response = await openai.chat.completions.create({
+      messages: [{ role: "system", content: prompt }],
+      model: "gpt-3.5-turbo",
+    });
+
+    const gptResponse = response.choices[0].message.content; 
+    const JsonResponse = JSON.stringify(gptResponse); 
+    
+
+    
+    return JsonResponse; 
+  } catch (error) {
+    console.error('Error connecting to OpenAI:', error);
+    throw error;
+  }
+}
+
+module.exports = sendPromptToOpenAI;
---
+++ server.js
@@ -0,0 +1,46 @@
+
+
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const app = express();
+const cors = require('cors'); // Require the CORS library
+const port = 3000;
+const extractTextFromPDF = require('./ExtractTextFromPDF'); 
+const generateResponse = require('./GenerateResponse'); 
+
+app.use(cors());
+// Set up storage for incoming files using multer
+const storage = multer.memoryStorage(); // Use memory storage to access file data
+
+const upload = multer({ storage: storage });
+
+
+
+// Endpoint to handle POST requests of PDF files
+app.post('/upload', upload.single('pdfFile'), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  const pdfUpload = req.file.buffer; 
+  
+  // Log the PDF file buffer to the console
+  console.log( 'this is the pdf file sent through',  req.file.buffer);
+
+    const extractedText = await extractTextFromPDF(pdfUpload); 
+
+    console.log('this is the extracted text from the SERVER'); 
+    const gptResponse = await generateResponse(extractedText); 
+
+    console.log('this is the extracted RESPONSE FROM GPT ON THE SERVER', gptResponse); 
+
+    res.end(gptResponse); 
+
+
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
